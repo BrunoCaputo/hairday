8d094568d6270ceddb4a6a0cc986319e45788887
BrunoCaputo <brunopc.dev@gmail.com>
2024-09-03 00:19:21 -0300

    feat: Handle submit and future dates

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 
 const form = document.querySelector("form");
+const clientName = document.getElementById("client");
 const selectedDate = document.getElementById("date");
 
 // Current date
@@ -14,4 +15,27 @@ selectedDate.min = todayDate;
 
 form.onsubmit = (event) => {
   event.preventDefault();
+
+  try {
+    const name = clientName.value.trim();
+
+    if (!name) {
+      return alert("Please enter the client's name");
+    }
+
+    const hourSelected = document.querySelector(".hour-selected");
+
+    if (!hourSelected) {
+      return alert("Please select a schedule time");
+    }
+
+    const [hour] = hourSelected.innerText.split(":");
+
+    const scheduleTime = dayjs(selectedDate.value).add(hour, "hour");
+
+    const id = new Date().getTime();
+  } catch (error) {
+    alert("Sorry! Something went wrong with your schedule. Please try again!");
+    console.error(error);
+  }
 };
